e3177490eba74bd4246757d3853753d49f7dec50
petr-nguyen <petr.nguyen@gusrix.com>
2022-10-03 16:43:47 +0700

    update addons

--- addons/mail_plugin/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-from . import authenticate
-from . import mail_plugin
--- addons/mail_plugin/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-from . import test_controller
-from . import test_res_partner_iap
